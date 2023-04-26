a3cecf568fd0db54cf6a8066d28385ea98187d6a
Chris James <chris@lattice.com>
2023-04-26 14:43:46 -0700

    Whoops, forgot build

--- dist/index.js
+++ dist/index.js
@@ -21517,18 +21517,6 @@ const { OWNER, REPO, SHA, octokit, jobName } = constants_1.default;
  * @return the check ID of the created run
  */
 async function fetchStatusCheck() {
-    //   const jobParameters = {
-    //     owner: OWNER,
-    //     repo: REPO,
-    //     run_id: Number(process.env['GITHUB_RUN_ID']),
-    //     per_page: 100,
-    //     headers: {
-    //       'X-GitHub-Api-Version': '2022-11-28',
-    //     },
-    //   }
-    //   const response = await octokit.paginate('GET /repos/{owner}/{repo}/actions/runs/{run_id}/jobs', jobParameters)
-    //   console.log('response', response)
-    //   console.log('jobs', response.jobs)
     const checkParameters = {
         owner: OWNER,
         repo: REPO,
@@ -21539,12 +21527,9 @@ async function fetchStatusCheck() {
     };
     for await (const response of octokit.paginate.iterator('GET /repos/{owner}/{repo}/commits/{ref}/check-runs', checkParameters)) {
         const currentCheck = response.data.find((check) => check.name === jobName);
-        console.log(currentCheck);
-        // for (const check of response.data.check_runs) {
-        //   console.log('check', check)
-        // }
-        // // Return the status check ID
-        // return response.data.check_runs?.[0].id as number
+        if (currentCheck) {
+            return currentCheck.id;
+        }
     }
     return;
 }
