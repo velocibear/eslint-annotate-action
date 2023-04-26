70e6dde6873d46c7c2bda6c30069087f7c1198b2
Chris James <chris@lattice.com>
2023-04-26 14:41:50 -0700

    Passing along id if found

--- src/fetchStatusCheck.ts
+++ src/fetchStatusCheck.ts
@@ -6,20 +6,6 @@ const {OWNER, REPO, SHA, octokit, jobName} = constants
  * @return the check ID of the created run
  */
 export default async function fetchStatusCheck(): Promise<number | undefined> {
-  //   const jobParameters = {
-  //     owner: OWNER,
-  //     repo: REPO,
-  //     run_id: Number(process.env['GITHUB_RUN_ID']),
-  //     per_page: 100,
-  //     headers: {
-  //       'X-GitHub-Api-Version': '2022-11-28',
-  //     },
-  //   }
-
-  //   const response = await octokit.paginate('GET /repos/{owner}/{repo}/actions/runs/{run_id}/jobs', jobParameters)
-  //   console.log('response', response)
-  //   console.log('jobs', response.jobs)
-
   const checkParameters = {
     owner: OWNER,
     repo: REPO,
@@ -33,13 +19,9 @@ export default async function fetchStatusCheck(): Promise<number | undefined> {
     checkParameters,
   )) {
     const currentCheck = response.data.find((check) => check.name === jobName)
-    console.log(currentCheck)
-
-    // for (const check of response.data.check_runs) {
-    //   console.log('check', check)
-    // }
-    // // Return the status check ID
-    // return response.data.check_runs?.[0].id as number
+    if (currentCheck) {
+      return currentCheck.id
+    }
   }
   return
 }
